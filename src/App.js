c969d488a58e42e52e85bd7bd05fac52f184d9d1
teslasdev <teslasdeveloper@gmail.com>
2024-06-30 00:45:54 +0100

    live

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import Navigation from './views/Navigation/Navigation'
-import './App.css'
-import Banner from './views/Banner/Banner'
-import Sections from './views/section/Sections'
-import Blog from './views/Blog/Blog'
-import Footer from './views/Footer/Footer'
-import './views/Responsive/Tablet.css';
+import React, { useEffect } from "react";
+import Navigation from "./views/Navigation/Navigation";
+import "./App.css";
+import Banner from "./views/Banner/Banner";
+import Sections from "./views/section/Sections";
+import Blog from "./views/Blog/Blog";
+import Footer from "./views/Footer/Footer";
+import "./views/Responsive/Tablet.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000, // Animation duration in milliseconds
+      easing: "ease-in-out", // Easing function
+      once: true, // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
   return (
     <div>
-      <Navigation/>
+      <Navigation />
       <Banner />
       <Sections />
       <Blog />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/views/component/VerticalSection.jsx
+++ src/views/component/VerticalSection.jsx
@@ -4,7 +4,7 @@ import './VerticalSection.css'
 
 const VerticalSection = (props) => {
   return (
-    <div className='projects-vertical'>
+    <div className='projects-vertical' data-aos="flip-up">
         <div className='ghKDMbi-vertical'>
             <div className='texts'>
               <h3>{props.title}</h3>
